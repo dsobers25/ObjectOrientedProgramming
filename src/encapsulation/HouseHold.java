f531f918bc8aa2bdbc62bc4489ef8dfb26cbb64f
Deron Sobers <deronfsobers2@gmail.com>
2019-06-22 15:36:18 -0500

    added comments to HouseHold and TestHouseHold class added a .gitignore file

--- src/encapsulation/HouseHold.java
+++ src/encapsulation/HouseHold.java
@@ -1,20 +1,25 @@
 package encapsulation;
 
 public class HouseHold {
+
+    // private instance variable to be returned
     private int kids;
     private int parents = 2;
 
+    // when this method is called the kids variable is incremented by one
     public void newKid() {
         kids++;
     }
 
+    // returns the kids variable
     public int getKids() {
         return kids;
     }
 
+    // returns the parents variable
     public int getParents() {
         return parents;
     }
 
 
-}
+}// End of class
--- src/encapsulation/TestHouseHold.java
+++ src/encapsulation/TestHouseHold.java
@@ -3,20 +3,25 @@ package encapsulation;
 public class TestHouseHold {
 
     public static void main(String[] args){
+
+        // Created an object named house from the HouseHold class
         HouseHold house = new HouseHold();
 
+        // any time a the newkid method is called this kids instance variable is incremented by one
         house.newKid();
-//        house.newKid();
-//        house.newKid();
-//        house.newKid();
+        house.newKid();
+        house.newKid();
+        house.newKid();
 
+        // if the kids instance variable is less than or equal to 1 then the prompt is singular
         if (house.getKids() <= 1) {
             System.out.println("There is " + house.getKids() + " kid in the house and " + house.getParents() + " parents in the household");
 
+            // else the prompt is plural
         } else {
             System.out.println("There are " + house.getKids() + " kids in the house and " + house.getParents() + " parents in the household");
         }
 
 
-    }
-}
+    }// End of main method
+}// End of class
