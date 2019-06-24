f375b9d57713f93fe7e6e4319c738a3571836b94
Deron Sobers <deronfsobers2@gmail.com>
2019-06-23 23:37:04 -0500

    added String knowledge and legacy variables in the WhatWillYouGet class

--- src/inheritance/WhatWillYouGet.java
+++ src/inheritance/WhatWillYouGet.java
@@ -4,5 +4,7 @@ public class WhatWillYouGet {
 
     String car = "2008 Ford Fusion";
     double money = 1000000;
+    String knowledge = "All the books I've ever read";
+    String legacy = "Sobers impact";
 
 }
